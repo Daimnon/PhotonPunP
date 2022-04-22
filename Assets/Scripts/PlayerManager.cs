8bf007a55448809f84374c3e223c8662fddaa4c5
Daimnon <galdrorerez@gmail.com>
2022-04-22 19:39:34 +0300

    Finito

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -14,8 +14,6 @@ namespace Net.DaimnonGames.PhotonMultiplayer
     {
 
         #region IPunObservable implementation
-
-
         public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
         {
             if (stream.IsWriting)
@@ -31,8 +29,6 @@ namespace Net.DaimnonGames.PhotonMultiplayer
                 this.Health = (float)stream.ReceiveNext();
             }
         }
-
-
         #endregion
 
         #region Private Fields
@@ -44,6 +40,18 @@ namespace Net.DaimnonGames.PhotonMultiplayer
         bool IsFiring;
         #endregion
 
+        #region Public Fields
+        [Tooltip("The current Health of our player")]
+        public float Health = 1f;
+        
+        [Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
+        public static GameObject LocalPlayerInstance;
+
+        [Tooltip("The Player's UI GameObject Prefab")]
+        [SerializeField]
+        public GameObject PlayerUiPrefab;
+        #endregion
+
         #region Private Method
 
 #if UNITY_5_4_OR_NEWER
@@ -53,15 +61,6 @@ namespace Net.DaimnonGames.PhotonMultiplayer
         }
 #endif
 
-        #endregion
-
-
-        #region Public Fields
-        [Tooltip("The current Health of our player")]
-        public float Health = 1f;
-        
-        [Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
-        public static GameObject LocalPlayerInstance;
         #endregion
 
         #region MonoBehaviour CallBacks
@@ -114,6 +113,14 @@ namespace Net.DaimnonGames.PhotonMultiplayer
             // Unity 5.4 has a new scene management. register a method to call CalledOnLevelWasLoaded.
             UnityEngine.SceneManagement.SceneManager.sceneLoaded += OnSceneLoaded;
 #endif
+
+            if (PlayerUiPrefab != null)
+            {
+                GameObject _uiGo = Instantiate(PlayerUiPrefab);
+                _uiGo.SendMessage("SetTarget", this, SendMessageOptions.RequireReceiver);
+            }
+            else
+                Debug.LogWarning("<Color=Red><a>Missing</a></Color> PlayerUiPrefab reference on player Prefab.", this);
         }
 
         /// <summary>
@@ -121,11 +128,8 @@ namespace Net.DaimnonGames.PhotonMultiplayer
         /// </summary>
         void Update()
         {
-
             if (photonView.IsMine)
-            {
                 ProcessInputs();
-            }
 
             // trigger Beams active state
             if (beams != null && IsFiring != beams.activeInHierarchy)
@@ -134,9 +138,7 @@ namespace Net.DaimnonGames.PhotonMultiplayer
                 {
                     ProcessInputs();
                     if (Health <= 0f)
-                    {
                         GameManager.Instance.LeaveRoom();
-                    }
                 }
 
                 beams.SetActive(IsFiring);
@@ -152,15 +154,13 @@ namespace Net.DaimnonGames.PhotonMultiplayer
         void OnTriggerEnter(Collider other)
         {
             if (!photonView.IsMine)
-            {
                 return;
-            }
+
             // We are only interested in Beamers
             // we should be using tags but for the sake of distribution, let's simply check by name.
             if (!other.name.Contains("Beam"))
-            {
                 return;
-            }
+
             Health -= 0.1f;
         }
         /// <summary>
@@ -172,15 +172,13 @@ namespace Net.DaimnonGames.PhotonMultiplayer
         {
             // we dont' do anything if we are not the local player.
             if (!photonView.IsMine)
-            {
                 return;
-            }
+
             // We are only interested in Beamers
             // we should be using tags but for the sake of distribution, let's simply check by name.
             if (!other.name.Contains("Beam"))
-            {
                 return;
-            }
+
             // we slowly affect health when beam is constantly hitting us, so player has to move to prevent death.
             Health -= 0.1f * Time.deltaTime;
         }
@@ -197,9 +195,10 @@ void OnLevelWasLoaded(int level)
         {
             // check if we are outside the Arena and if it's the case, spawn around the center of the arena in a safe zone
             if (!Physics.Raycast(transform.position, -Vector3.up, 5f))
-            {
                 transform.position = new Vector3(0f, 5f, 0f);
-            }
+
+            GameObject _uiGo = Instantiate(this.PlayerUiPrefab);
+            _uiGo.SendMessage("SetTarget", this, SendMessageOptions.RequireReceiver);
         }
 
 #if UNITY_5_4_OR_NEWER
--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -1,18 +1,106 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class PlayerUI : MonoBehaviour
+
+using System.Collections;
+
+
+namespace Net.DaimnonGames.PhotonMultiplayer
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerUI : MonoBehaviour
     {
-        
-    }
+        #region Private Fields
+        [Tooltip("UI Text to display Player's Name")]
+        [SerializeField]
+        private TextMeshProUGUI playerNameText;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [Tooltip("UI Slider to display Player's Health")]
+        [SerializeField]
+        private Slider playerHealthSlider;
+
+        private PlayerManager target;
+
+        private float characterControllerHeight = 0f;
+        private Transform targetTransform;
+        private Renderer targetRenderer;
+        private CanvasGroup _canvasGroup;
+        private Vector3 targetPosition;
+        #endregion
+
+        #region Public Field
+        [Tooltip("Pixel offset from the player target")]
+        [SerializeField]
+        private Vector3 screenOffset = new Vector3(0f, 30f, 0f);
+        #endregion
+
+        #region MonoBehaviour Callbacks
+        private void Awake()
+        {
+            transform.SetParent(GameObject.Find("Canvas").GetComponent<Transform>(), false);
+            _canvasGroup = GetComponent<CanvasGroup>();
+        }
+
+        private void Update()
+        {
+            // Reflect the Player Health
+            if (playerHealthSlider != null)
+                playerHealthSlider.value = target.Health;
+
+            // Destroy itself if the target is null, It's a fail safe when Photon is destroying Instances of a Player over the network
+            if (target == null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+        }
+
+        private void LateUpdate()
+        {
+            // Do not show the UI if we are not visible to the camera, thus avoid potential bugs with seeing the UI, but not the player itself.
+            if (targetRenderer != null)
+            {
+                _canvasGroup.alpha = targetRenderer.isVisible ? 1f : 0f;
+            }
+
+
+            // #Critical
+            // Follow the Target GameObject on screen.
+            if (targetTransform != null)
+            {
+                targetPosition = targetTransform.position;
+                targetPosition.y += characterControllerHeight;
+                transform.position = Camera.main.WorldToScreenPoint(targetPosition) + screenOffset;
+            }
+        }
+        #endregion
+
+        #region Public Methods
+        public void SetTarget(PlayerManager _target)
+        {
+            if (_target == null)
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> PlayMakerManager target for PlayerUI.SetTarget.", this);
+                return;
+            }
+
+            // Cache references for efficiency
+            target = _target;
+
+            targetTransform = this.target.GetComponent<Transform>();
+            targetRenderer = this.target.GetComponent<Renderer>();
+            CharacterController characterController = _target.GetComponent<CharacterController>();
+
+            // Get data from the Player that won't change during the lifetime of this Component
+            if (characterController != null)
+                characterControllerHeight = characterController.height;
+
+            if (playerNameText != null)
+                playerNameText.text = target.photonView.Owner.NickName;
+                
+        }
+        #endregion
+
+
     }
 }
